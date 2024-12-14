d6fbbbafde55bee8e9be20754800f3c74d12ce10
tomib4n <tbanchio15@gmail.com>
2024-12-14 11:32:27 -0300

    feat: instalacion de dependencias de desarrollo

---
+++ eslint.config.mjs
@@ -0,0 +1,57 @@
+// eslint-disable-next-line unicorn/import-style
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:unicorn/recommended',
+    'plugin:import/recommended',
+    'plugin:playwright/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/no-array-for-each': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            e2e: true,
+          },
+          replacements: {
+            props: false,
+            ref: false,
+            params: false,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ['*.js'],
+    rules: {
+      'unicorn/prefer-module': 'off',
+    },
+  },
+];
+
+export default eslintConfig;
