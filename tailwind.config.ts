76c2fff732c3928468841eebcbdec028f81aa9b6
b4n <81537786+tomiban@users.noreply.github.com>
2025-01-02 13:03:51 -0300

    Revert "Implementa sidebar component"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,16 +50,6 @@ export default {
           '4': 'hsl(var(--chart-4))',
           '5': 'hsl(var(--chart-5))',
         },
-        sidebar: {
-          DEFAULT: 'hsl(var(--sidebar-background))',
-          foreground: 'hsl(var(--sidebar-foreground))',
-          primary: 'hsl(var(--sidebar-primary))',
-          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-          accent: 'hsl(var(--sidebar-accent))',
-          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-          border: 'hsl(var(--sidebar-border))',
-          ring: 'hsl(var(--sidebar-ring))',
-        },
       },
       borderRadius: {
         lg: 'var(--radius)',
@@ -68,6 +58,5 @@ export default {
       },
     },
   },
-  // eslint-disable-next-line @typescript-eslint/no-require-imports, unicorn/prefer-module
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
