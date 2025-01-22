5e48c955a85adc664c43dcad9bbec798ee377129
tomib4n <tbanchio15@gmail.com>
2025-01-22 09:50:19 -0300

    feat(auth): implementacion de login y registro con google y credenciales
    - Agrega schema de validacion con zod -Implementa server actions para registrarse y logearse

---
+++ src/lib/auth.ts
@@ -0,0 +1,64 @@
+import bcryptjs from 'bcryptjs';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import Google from 'next-auth/providers/google';
+
+import { LoginSchema } from '@/schema/auth';
+
+import { authConfig } from './auth.config';
+import database from './db';
+
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth({
+  ...authConfig,
+  providers: [
+    Google,
+    Credentials({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const { data, success } = LoginSchema.safeParse(credentials);
+
+        //verificamos si loos datos llegaron bien del front
+        if (!success) {
+          throw new Error('Credenciales incorrectas');
+        }
+        //verificamos si existe el usuario
+        const user = await database.user.findFirst({
+          where: {
+            email: data.email,
+          },
+          select: {
+            id: true,
+            email: true,
+            name: true,
+            password: true,
+          },
+        });
+
+        if (!user || !user.password) {
+          throw new Error('Credenciales incorrectas');
+        }
+
+        //verificamos si la contraseña es correcta
+        const passwordMatch = await bcryptjs.compare(
+          data.password,
+          user.password,
+        );
+
+        if (!passwordMatch) {
+          throw new Error('Credenciales incorrectas');
+        }
+
+        return user;
+      },
+    }),
+  ],
+});
---
+++ src/schema/auth.ts
@@ -0,0 +1,49 @@
+import { object, string, z } from 'zod';
+
+export const LoginSchema = object({
+  email: string({
+    required_error: 'Email requerido!',
+  })
+    .trim()
+    .min(1, 'Email requerido!')
+    .email('Email inválido!'),
+  password: string({
+    required_error: 'Contraseña requerida!',
+  })
+    .trim()
+    .min(1, 'Contraseña requerida!')
+    .min(8, 'La contraseña debe tener al menos 8 caracteres!'),
+});
+
+export const RegisterSchema = object({
+  name: string({
+    required_error: 'Nombre requerido!',
+  })
+    .trim()
+    .min(2, 'El nombre debe tener al menos 2 caracteres!')
+    .max(50, 'El nombre no puede tener más de 50 caracteres!'),
+  email: string({
+    required_error: 'Email requerido!',
+  })
+    .trim()
+    .min(1, 'Email requerido!')
+    .email('Email inválido!'),
+  password: string({
+    required_error: 'Contraseña requerida!',
+  })
+    .trim()
+    .min(1, 'Contraseña requerida!')
+    .min(6, 'La contraseña debe tener al menos 6 caracteres!')
+    .max(50, 'La contraseña no puede tener más de 50 caracteres!'),
+  confirmPassword: string({
+    required_error: 'Confirmación de contraseña requerida!',
+  })
+    .trim()
+    .min(1, 'Confirmación de contraseña requerida!'),
+}).refine(data => data.password === data.confirmPassword, {
+  message: 'Las contraseñas no coinciden!',
+  path: ['confirmPassword'],
+});
+
+export type TLoginSchema = z.infer<typeof LoginSchema>;
+export type TRegisterSchema = z.infer<typeof RegisterSchema>;
