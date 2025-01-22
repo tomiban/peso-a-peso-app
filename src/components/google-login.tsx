7ec3bdb71b5f56e9f13128ff6ab387d30f1aa7ca
tomib4n <tbanchio15@gmail.com>
2025-01-22 10:52:06 -0300

    refactor(auth): mueve funcion de login con google a actions.ts

--- src/components/google-login.tsx
+++ src/components/google-login.tsx
@@ -1,22 +1,37 @@
+'use client';
+import { useState } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Google } from '@/components/ui/google';
-import { signIn } from '@/lib/auth';
-//import { signIn } from '@/lib/auth';
+import { googleLogin } from '@/lib/actions';
 
-const GoogleLogin = () => {
+import { Alert, AlertDescription } from './ui/alert';
+
+export const GoogleLogin = () => {
+  const [error, setError] = useState<string | null>(null);
+
   return (
-    <form
-      action={async () => {
-        'use server';
-        await signIn('google');
-      }}
-    >
-      <Button className="glass-card w-full" variant="outline">
-        <Google size={20} />
-        Accede con Google
-      </Button>
-    </form>
+    <div>
+      <form
+        action={async () => {
+          setError(null);
+          try {
+            await googleLogin();
+          } catch {
+            setError('Error al iniciar sesión con Google');
+          }
+        }}
+      >
+        <Button className="glass-card w-full" variant="outline">
+          <Google size={20} />
+          Accede con Google
+        </Button>
+      </form>
+      {error && (
+        <Alert variant="destructive">
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+    </div>
   );
 };
-
-export { GoogleLogin };
